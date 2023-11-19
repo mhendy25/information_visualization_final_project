5fdace91898ca3dca3bffec9d6641ab399448027
Dany <nohonetdany@gmail.com>
2023-11-19 22:35:54 +0800

    added the map of europe

--- final_project/src/airportMap.js
+++ final_project/src/airportMap.js
@@ -6,19 +6,19 @@ import { Routes } from './routes'
 function AirportMap(props){
     const {width, height, countries, airports, routes, selectedAirline} = props;
     let projection = geoMercator();//TODO: Create a projection of type Mercator.
-    projection.scale(97)
-            .translate([width / 2, height / 2 + 20]);
+    projection.scale(545)
+            .translate([width / 2, height + 300]);
     let path = geoPath().projection(projection);
     return <g>
         {
             countries.features.map(d => <path key={d.properties.name} d={path(d)} 
             stroke={"#ccc"} fill={"#eee"}></path>)
         }
-        {
+        {/* {
             airports.map(d => <circle key={d.AirportID} cx={projection([d.Longitude, d.Latitude])[0]}
                     cy={projection([d.Longitude, d.Latitude])[1]} r={1} fill={"#2a5599"}></circle>)
-        }
-        <Routes projection={projection} routes={routes} selectedAirline={selectedAirline}/>
+        } */}
+        {/* <Routes projection={projection} routes={routes} selectedAirline={selectedAirline}/> */}
     </g>
 
 
--- final_project/src/index.js
+++ final_project/src/index.js
@@ -9,23 +9,23 @@ import { AirportBubble} from "./airportBubble";
 
 
 const csvUrl = 'https://gist.githubusercontent.com/hogwild/9367e694e12bd2616205e4b3e91285d5/raw/9b451dd6bcc148c3553f550c92096a1a58e1e1e5/airline-routes.csv';
-const mapUrl = 'https://gist.githubusercontent.com/hogwild/26558c07f9e4e89306f864412fbdba1d/raw/5458902712c01c79f36dc28db33e345ee71487eb/countries.geo.json';
+const mapUrl = 'https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson';
 
-function useData(csvPath){
-    const [dataAll, setData] = React.useState(null);
-    React.useEffect(() => {
-        csv(csvPath).then(data => {
-            data.forEach(d => {
-                d.SourceLatitude = +d.SourceLatitude
-                d.SourceLongitude = +d.SourceLongitude
-                d.DestLatitude = +d.DestLatitude
-                d.DestLongitude = +d.DestLongitude
-            });
-            setData(data);
-        });
-    }, []);
-    return dataAll;
-}
+// function useData(csvPath){
+//     const [dataAll, setData] = React.useState(null);
+//     React.useEffect(() => {
+//         csv(csvPath).then(data => {
+//             data.forEach(d => {
+//                 d.SourceLatitude = +d.SourceLatitude
+//                 d.SourceLongitude = +d.SourceLongitude
+//                 d.DestLatitude = +d.DestLatitude
+//                 d.DestLongitude = +d.DestLongitude
+//             });
+//             setData(data);
+//         });
+//     }, []);
+//     return dataAll;
+// }
 
 function useMap(jsonPath) {
     const [data, setData] = React.useState(null);
@@ -39,32 +39,36 @@ function useMap(jsonPath) {
 
 
 function AirlineRoutes(){
-    const [selectedAirline, setSelectedAirline]=React.useState(null);
+    // const [selectedAirline, setSelectedAirline]=React.useState(null);
     const barchart_width = 350;
     const barchart_height = 400;
     const barchart_margin = { top: 10, bottom: 50, left: 130, right: 10 };
     const barchart_inner_width = barchart_width - barchart_margin.left - barchart_margin.right;
     const barchart_inner_height = barchart_height - barchart_margin.top - barchart_margin.bottom;
     const map_width = 600;
-    const map_height = 400;
+    const map_height = 700;
     const hub_width = 400;
     const hub_height = 400;
 
-    const routes = useData(csvUrl);
+    // const routes = useData(csvUrl);
     const map = useMap(mapUrl);
     
-    if (!map || !routes) {
+    // if (!map || !routes) {
+    //     return <pre>Loading...</pre>;
+    // };
+
+    if (!map) {
         return <pre>Loading...</pre>;
     };
-    let airlines = groupByAirline(routes);
-    let airports = groupByAirport(routes);
+    // let airlines = groupByAirline(routes);
+    // let airports = groupByAirport(routes);
     // console.log(airlines);
     // console.log(airports);
     // console.log(routes);
     return <div>
-        <h1>Airlines Routes</h1>   
+        <h1>UEFA CHAMPIONS LEAGUE – a visual narrative</h1>   
         <div className={"mainView"}>
-            <div>
+            {/* <div>
                 <h2>Airlines</h2>
                 <svg id={"barchart"} width={barchart_width} height={barchart_height}>
                     <BarChart offsetX={barchart_margin.left} offsetY={barchart_margin.top} 
@@ -72,17 +76,19 @@ function AirlineRoutes(){
                         selectedAirline={selectedAirline} setSelectedAirline={setSelectedAirline}
                     />
                 </svg>
-            </div>
-            <div>
-                <h2>Airports</h2>
-                <svg id={"map"} width={map_width} height={map_height}>
-                    <AirportMap width={map_width} height={map_height} 
-                        countries={map} airports={airports} routes={routes}
-                        selectedAirline={selectedAirline}
+            </div> */}
+            <div className="full-width-container">
+                {/* <h2>UEFA Champions League</h2> */}
+                {/* </div>width={map_width}  */}
+                <svg id={"map"} height={map_height} width={window.innerWidth/2}>
+                    <AirportMap width={window.innerWidth/3} height={map_height} countries={map}
+                        //  airports={airports} routes={routes}
+                        // selectedAirline={selectedAirline}
                     />
                 </svg>
+
             </div>
-            <div>
+            {/* <div>
                 <h2>The Hubs</h2>
                 <svg id={"bubble"} width={hub_width} height={hub_height}>
                     <AirportBubble width={hub_width} height={hub_height} 
@@ -91,9 +97,22 @@ function AirlineRoutes(){
                     />
                 </svg>
 
+            </div> */}
+
+            
+            <div>
+                <svg id={"view1"} height={map_height/2} width={window.innerWidth/2} style={{ position: 'absolute', left: (window.innerWidth / 2)+10, top:(map_height/2)+90 }}>
+                </svg>
+            </div>
+
+            <div>
+                <svg id={"view2"} height={map_height/2} width={window.innerWidth/2}>
+                </svg>
             </div>
             
         </div>
+
+        
     </div>
 }
 
