c17ef356d1962c07e01009eda2cf04b828a13af2
Dany <nohonetdany@gmail.com>
2023-11-21 15:14:05 +0800

    added some points on the map

--- final_project/src/index.js
+++ final_project/src/index.js
@@ -3,29 +3,49 @@ import ReactDOM from "react-dom";
 import { csv, json } from "d3";
 import { groupByAirline, groupByAirport } from "./utils";
 import "./styles.css";
-import { AirportMap } from "./airportMap";
+import { ClubMap } from "./uefaClubMap";
 import { BarChart } from "./barChart";
 import { AirportBubble} from "./airportBubble";
 
 
-const csvUrl = 'https://gist.githubusercontent.com/hogwild/9367e694e12bd2616205e4b3e91285d5/raw/9b451dd6bcc148c3553f550c92096a1a58e1e1e5/airline-routes.csv';
+// The csv file with the club rankings in the competition (uses fifa country codes)
+const csvUrl = 'https://gist.githubusercontent.com/danysigha/f3e5054ce147833a899178553eb2d883/raw/f27858668ec440a9e85745a6b827b3ebd8fd6185/alltimerankingbyclub.csv';
+
+// The geojson of the map of Europe
 const mapUrl = 'https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson';
 
-// function useData(csvPath){
-//     const [dataAll, setData] = React.useState(null);
-//     React.useEffect(() => {
-//         csv(csvPath).then(data => {
-//             data.forEach(d => {
-//                 d.SourceLatitude = +d.SourceLatitude
-//                 d.SourceLongitude = +d.SourceLongitude
-//                 d.DestLatitude = +d.DestLatitude
-//                 d.DestLongitude = +d.DestLongitude
-//             });
-//             setData(data);
-//         });
-//     }, []);
-//     return dataAll;
-// }
+// The mapping between fifa country codes and the country names
+const codesUrl = 'https://gist.githubusercontent.com/danysigha/774a124bc279c56ed4323802dafd6445/raw/200692a0c0941e3c8086fb94e9a3436f131cd337/fifa_codes.csv'; 
+
+function useRankingsData(csvPath){
+    const [dataAll, setData] = React.useState(null);
+    React.useEffect(() => {
+        csv(csvPath).then(data => {
+            data.forEach(d => {
+                d.Titles = +d.Titles
+                d.Pts = +d.Pts
+                d.Position = +d.Position
+                d.Win = +d.Win
+                d.Draw = +d.Draw
+                d.Loss = +d.Loss
+                d.Played = +d.Played
+                d.Participated = +d.Participated
+            });
+            setData(data);
+        });
+    }, []);
+    return dataAll;
+}
+
+function useFifaData(csvPath){
+    const [dataAll, setData] = React.useState(null);
+    React.useEffect(() => {
+        csv(csvPath).then(data => {
+            setData(data);
+        });
+    }, []);
+    return dataAll;
+}
 
 function useMap(jsonPath) {
     const [data, setData] = React.useState(null);
@@ -38,68 +58,102 @@ function useMap(jsonPath) {
 }
 
 
-function AirlineRoutes(){
-    // const [selectedAirline, setSelectedAirline]=React.useState(null);
-    const barchart_width = 350;
-    const barchart_height = 400;
-    const barchart_margin = { top: 10, bottom: 50, left: 130, right: 10 };
-    const barchart_inner_width = barchart_width - barchart_margin.left - barchart_margin.right;
-    const barchart_inner_height = barchart_height - barchart_margin.top - barchart_margin.bottom;
-    const map_width = 600;
+function UefaClubs(){
     const map_height = 700;
-    const hub_width = 400;
-    const hub_height = 400;
-
-    // const routes = useData(csvUrl);
+    const rankings = useRankingsData(csvUrl);
+    const fifa_codes = useFifaData(codesUrl);
     const map = useMap(mapUrl);
-    
-    // if (!map || !routes) {
-    //     return <pre>Loading...</pre>;
-    // };
 
-    if (!map) {
+    if (!map || !rankings) {
         return <pre>Loading...</pre>;
     };
-    // let airlines = groupByAirline(routes);
-    // let airports = groupByAirport(routes);
-    // console.log(airlines);
-    // console.log(airports);
-    // console.log(routes);
+
+    // Group clubs by the "Country" attribute
+    const groupedArray = rankings.reduce((acc, member) => {
+        const { Country, ...rest } = member;
+        if (member.Position <= 100){
+            // Check if the country key already exists in the accumulator
+            if (!acc[Country]) {
+                // If not, create a new key with an array containing the current member
+                acc[Country] = [rest];
+            } else {
+                // If yes, push the current member to the existing array
+                acc[Country].push(rest);
+            }
+        }
+        return acc;
+    }, {});
+
+    // Convert the grouped data into an array of key-value pairs
+    const groupedClubs = Object.entries(groupedArray);
+
+
+    const code_country_map = fifa_codes.reduce((acc, member) => {
+        const { FIFA, CLDR_display_name } = member;
+        
+        // Check if the country key already exists in the accumulator
+        if (!acc[FIFA]) {
+            if (FIFA == "ENG,NIR,SCO,WAL"){
+                acc["ENG"] = "United Kingdom";
+                acc["NIR"] = "Ireland";
+                acc["WAL"] = "Wales";
+                acc["CZE"] = "Czech Republic";
+            }else{
+                acc[FIFA] = CLDR_display_name;
+            }
+            // If not, create a new key with an array containing the current member
+        }
+        return acc;
+    }, {});
+
+    const club_country_pairs = [];
+    // create an array of club and country pairs (ie. Spain appears more than once)
+    for (const [key, value] of Object.entries(groupedClubs)) {
+        var club_fifa_code = value[0];
+        var clubs = value[1];
+        
+        if(code_country_map[club_fifa_code]){
+            for (var club in clubs){
+                var club_name = clubs[club]["Club"];
+                club_country_pairs.push( { "country": code_country_map[value[0]], "club":club_name } );
+            }
+        }
+    }
+
+    // create an array of objects with country names their longitudes and their lattitudes FOR EACH CLUB
+    const longitudes_lattitudes = [];
+
+    map["features"].forEach( (element) =>{
+        if (element.hasOwnProperty("properties")) {
+            var country = element["properties"]["NAME"];
+            var lon = element["properties"]["LON"];
+            var lat = element["properties"]["LAT"];
+
+            club_country_pairs.forEach( (element) =>{
+                if (country.includes(element["country"])){
+                    longitudes_lattitudes.push( {"country": country, "longitude":lon, "latitude":lat});
+                }
+            })
+        }
+    }) 
+
+    // console.log(longitudes_lattitudes);
+
     return <div>
         <h1>UEFA CHAMPIONS LEAGUE – a visual narrative</h1>   
         <div className={"mainView"}>
-            {/* <div>
-                <h2>Airlines</h2>
-                <svg id={"barchart"} width={barchart_width} height={barchart_height}>
-                    <BarChart offsetX={barchart_margin.left} offsetY={barchart_margin.top} 
-                        height={barchart_inner_height} width={barchart_inner_width} data={airlines}
-                        selectedAirline={selectedAirline} setSelectedAirline={setSelectedAirline}
-                    />
-                </svg>
-            </div> */}
+            
             <div className="full-width-container">
-                {/* <h2>UEFA Champions League</h2> */}
-                {/* </div>width={map_width}  */}
+                
                 <svg id={"map"} height={map_height} width={window.innerWidth/2}>
-                    <AirportMap width={window.innerWidth/3} height={map_height} countries={map}
-                        //  airports={airports} routes={routes}
-                        // selectedAirline={selectedAirline}
+                    <ClubMap width={window.innerWidth/3} height={map_height} countries={map} 
+                    selectedCountries={longitudes_lattitudes}
                     />
                 </svg>
 
             </div>
-            {/* <div>
-                <h2>The Hubs</h2>
-                <svg id={"bubble"} width={hub_width} height={hub_height}>
-                    <AirportBubble width={hub_width} height={hub_height} 
-                        countries={map}  routes={routes}
-                        selectedAirline={selectedAirline}
-                    />
-                </svg>
-
-            </div> */}
-
             
+
             <div>
                 <svg id={"view1"} height={map_height/2} width={window.innerWidth/2} style={{ position: 'absolute', left: (window.innerWidth / 2)+10, top:(map_height/2)+90 }}>
                 </svg>
@@ -112,8 +166,7 @@ function AirlineRoutes(){
             
         </div>
 
-        
     </div>
 }
 
-ReactDOM.render(<AirlineRoutes/ >, document.getElementById("root"));
+ReactDOM.render(<UefaClubs/ >, document.getElementById("root"));
--- final_project/src/uefaClubMap.js
+++ final_project/src/uefaClubMap.js
@@ -2,26 +2,29 @@ import React from "react";
 import { geoPath, geoMercator } from "d3-geo";
 import { Routes } from './routes'
 
+var step = 10;
 
-function AirportMap(props){
-    const {width, height, countries, airports, routes, selectedAirline} = props;
+function ClubMap(props){
+    const {width, height, countries, selectedCountries} = props;
     let projection = geoMercator();//TODO: Create a projection of type Mercator.
     projection.scale(545)
             .translate([width / 2, height + 300]);
     let path = geoPath().projection(projection);
+
     return <g>
         {
             countries.features.map(d => <path key={d.properties.name} d={path(d)} 
             stroke={"#ccc"} fill={"#eee"}></path>)
         }
-        {/* {
-            airports.map(d => <circle key={d.AirportID} cx={projection([d.Longitude, d.Latitude])[0]}
-                    cy={projection([d.Longitude, d.Latitude])[1]} r={1} fill={"#2a5599"}></circle>)
-        } */}
-        {/* <Routes projection={projection} routes={routes} selectedAirline={selectedAirline}/> */}
-    </g>
+        {   
+            selectedCountries.map(
+                d => <circle cx={projection([d.longitude, d.latitude])[0]  }
+                    cy={projection([d.longitude, d.latitude])[1] + Math.floor(Math.random() * 5) } r={2} fill={"#2a5599"}></circle>
+            )
+        }
 
+    </g>
 
 }
 
-export { AirportMap }
+export { ClubMap }
